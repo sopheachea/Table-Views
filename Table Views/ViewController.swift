5170f9e3718f8bb6ceb2453832a0b3852ec79fe9
sophea_chea <sophea@sopheas-Mac.local>
2016-11-19 23:09:38 +0700

    Initial Commit

---
+++ Table Views/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table Views
+//
+//  Created by sophea chea on 11/19/16.
+//  Copyright © 2016 sophea chea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
